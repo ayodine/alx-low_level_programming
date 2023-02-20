80f1fb9175d2589eea23b803f3f3ec742cdafd9b
ayodine <ayodine47@gmail.com>
2023-02-19 19:13:50 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Hexadecimal
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i +48);
+	}
+	for (i = 10; i < 16; i++)
+	{
+		putchar(i + 87);
+	}
+	putchar('\n');
+	return (0);
+}
