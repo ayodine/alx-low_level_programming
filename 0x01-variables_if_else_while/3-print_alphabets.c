eff596b52e580e9e4f54861103cdbfa429035447
ayodine <ayodine47@gmail.com>
2023-02-19 18:26:36 -0800

    AlphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
+
 /**
  * main - alphabet lower and upper
  * Return: Always 0 (success)
  */
 
-int main() {
-    char lowercase, uppercase;
+int main(void)
+{
+	int i;
 
-    putchar('Lowercase alphabet: ');
-    for(lowercase = 'a'; lowercase <= 'z'; lowercase++) {
-        putchar(lowercase);
-        putchar(' ');
-    }
-
-    putchar('\n');
-
-    putchar('Uppercase alphabet: ');
-    for(uppercase = 'A'; uppercase <= 'Z'; uppercase++) {
-        putchar(uppercase);
-        putchar(' ');
-    }
-
-    putchar('\n');
-
-    return 0;
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar('A' + i);
+	}
+	putchar('\n');
+	return (0);
 }
-
