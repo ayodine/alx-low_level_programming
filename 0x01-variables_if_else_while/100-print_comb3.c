3026b80e42312433f2250658f1a3d23fa73570b5
ayodine <ayodine47@gmail.com>
2023-02-19 19:51:49 -0800

    combination of two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print digit from 0-10
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 90)
+	{
+		int j = i % 10;
+		int k = i / 10;
+
+		if (k != j)
+		{
+			putchar(k + 48);
+			putchar(j + 48);
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
