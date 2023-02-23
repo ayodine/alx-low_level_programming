d9cc9440c4d7c74d65bef686487256019d565ef0
ayodine <ayodine47@gmail.com>
2023-02-23 15:05:09 -0800

    check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - lowercase character
+ * @c: character for checking
+ * Return: 1 for lower case or 0 for anything else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 132)
+	{
+		return (1);
+	}
+	return (0);
+}
