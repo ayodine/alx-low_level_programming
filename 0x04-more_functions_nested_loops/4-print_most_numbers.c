9c494acec00e209bf928e96374213aeddbfae072
ayodine <ayodine47@gmail.com>
2023-02-23 16:28:53 -0800

    print number xclude 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	for (; c <= 9; c++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
+
+
