7260cf7d98e68e81fe5ebac553d4feaa7e41ada7
ayodine <ayodine47@gmail.com>
2023-02-28 18:06:45 -0800

    i can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,17 +4,18 @@
  * @s: string
  * return: 0
  */
+
 void print_rev(char *s)
 {
 	int c = 0;
 
 	while (s[c] != '\0')
 	{
-		c++
+		c++;
 	}
 	for (c -= 1; c >= 0; c--)
 	{
-		_putchar(s)[c]);
+		_putchar((s)[c]);
 	}
 	_putchar('\n');
 }
